c3d1df445772893c07c80a82b1b2dd75f5bf8d2f
paulosell <paulosell@users.noreply.github.com>
2019-03-10 18:14:04 -0300

    lab07

---
+++ Lab-07-GPIO-and-External-Interrupts-part-2/Lab-07-GPIO-and-External-Interrupts-part-2.ino
@@ -0,0 +1,43 @@
+int interrupt_source = 0;
+volatile boolean led_state = 0;
+int debounce_limit = 200;
+int sensitivity = HIGH;
+int botao = 2;
+unsigned long antes = 0;
+unsigned long agora = 0;
+void setup() {
+  // put your setup code here, to run once:
+  attachInterrupt(digitalPinToInterrupt(botao), handler, HIGH);
+  pinMode(botao, INPUT);
+  pinMode(13, OUTPUT);
+  antes = millis();
+}
+
+void loop() {
+  digitalWrite(13, led_state);
+  
+}
+
+void handler(){
+  
+  if(debounce()){
+   if(digitalRead(botao) == LOW){
+      led_state = true;
+     
+    }else{
+      led_state = false;
+    }
+  }
+  
+}
+
+int debounce(){
+   agora = millis();
+  if(( agora - antes) > debounce_limit){
+    antes = agora;
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
