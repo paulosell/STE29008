7279a3701c1036b9dc4d699d1d61f377c7da1808
Paulo Sell <sellpaulof@gmail.com>
2019-03-26 22:32:52 -0300

    exec3

--- AV1/main.cpp
+++ AV1/main.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <avr/io.h>
+#include <math.h>
+#include <stdio.h>
 #define FOSC 16000000
 #define BAUD 9600
 #define myubrr (FOSC/16/BAUD)-1
@@ -33,6 +35,93 @@ void setup2(){
 	UCSR0C |= (3 << UCSZ00);
 
 }
+
+void setup3(){
+	ADCSRA |= (1 << ADPS2);
+	ADCSRA |= (1 << ADPS1);
+	ADCSRA |= (1 << ADPS0);
+
+	ADMUX  |= (1 << REFS0);
+	ADCSRA |= (1 << ADEN);
+
+}
+
+float read_adc(uint8_t channel){
+	ADMUX &= 0xE0;           //Clear bits MUX0-4
+	ADMUX |= channel&0x07;   //Defines the new ADC channel to be read by setting bits MUX0-2
+	ADCSRB = channel&(1<<3); //Set MUX5
+	ADCSRA |= (1<<ADSC);      //Starts a new conversion
+	while(ADCSRA & (1<<ADSC));  //Wait until the conversion is done
+	return ADCW;
+
+}         //Returns the ADC value of the chosen channel
+
+
+int RMS (int repeat){
+
+	float accumulated = 0;
+	float average;
+	float digital_value;
+	uint8_t channel = 5;
+	for (int i = 0; i < repeat; i++){
+		digital_value = read_adc(channel);
+		accumulated = accumulated + (digital_value*digital_value);
+
+
+	}
+
+	average = accumulated/repeat;
+
+	return sqrt(average);
+
+
+}
+void exec3(){
+	setup3();
+	setup2();
+
+	float val = RMS(30);
+	float analog_val = (val*5)/1024;
+	printf("%.f", double(val));
+	char a[10] = "Digital: ";
+	char b[12] = "Analogico: ";
+
+	for (int i = 0; i < 10; i ++){
+
+		while ( !( UCSR0A & (1<<UDRE0)) )  ;
+		UDR0 = (uint8_t) a[i];
+
+
+	}
+
+	while ( !( UCSR0A & (1<<UDRE0)) )  ;
+	UDR0 = (uint8_t)val + 0x30;
+
+	for (int i = 0; i < 12; i ++){
+
+		while ( !( UCSR0A & (1<<UDRE0)) )  ;
+		UDR0 = (uint8_t) b[i];
+
+
+	}
+
+
+	while ( !( UCSR0A & (1<<UDRE0)) )  ;
+	UDR0 = (uint8_t)analog_val;
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
 void controleBotao5V(){
 	// O led conectado no bit 6 da PORTH irá acender
 	// Quando o quinto bit da PORT4 estiver em 1
@@ -86,9 +175,14 @@ void exec2(){
 
 int main(){
 	//exec1();
-	exec2();
+	//exec2();
+
+
+	exec3();
+
 
 }
+
 
 
 
