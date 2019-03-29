12949e0d262bc192215deee99647a4853e47d806
Paulo Sell <sellpaulof@gmail.com>
2019-03-29 12:22:40 -0300

    confirmando os exercicios

--- AV1/main.cpp
+++ AV1/main.cpp
@@ -10,58 +10,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <avr/interrupt.h>
-#include <time.h>
+//#include <time.h>
 #include <avr/delay.h>
 
 #define FOSC 16000000
 #define BAUD 9600
-#define myubrr (FOSC/16/BAUD)-1
+
 
 
 void setup1(){
 
-	DDRE &= ~(1 << 4); // PORT D2 AS INPUT
-	DDRE &= ~(1 << 5); // PORT D3 AS INPUT
+	DDRE &= ~(1 << PE4); // PORT D2 COMO ENTRADA
+	DDRE &= ~(1 << PE5); // PORT D3 COMO ENTRADA
 
-	DDRH |= (1 << 5); // PORT D8 AS OUTPUT
-	DDRH |= (1 << 6); // PORT D9 AS OUTPUT
+	DDRH |= (1 << PH5);  // PORT D8 COMO SAIDA
+	DDRH |= (1 << PH6);  // PORT D9 COMO SAIDA
 
 }
 
 
 void setup2(){
-	unsigned int ubrr = (FOSC/16/BAUD)-1;
+	unsigned int ubrr = (FOSC/16/BAUD)-1;  // Definindo frequencia de oscilamento
 
-	UBRR0H = (unsigned char) (ubrr >> 8);
-	UBRR0L = (unsigned char) ubrr;
+	UBRR0H = (unsigned char) (ubrr >> 8);   // Definindo frequencia de
+	UBRR0L = (unsigned char) ubrr;		   // Oscilamento
 
-	UCSR0B |= (1 << RXEN0);
-	UCSR0B |= (1 << TXEN0);
+	UCSR0B |= (1 << RXEN0);				   // Habilitando RX0
+	UCSR0B |= (1 << TXEN0);				   // Habilitando TX0
 
-	UCSR0C &= ~(1 << USBS0);
-	UCSR0C |= (3 << UCSZ00);
+	UCSR0C &= ~(1 << USBS0);			   // Definindo numerido de bits e
+	UCSR0C |= (3 << UCSZ00);			   // Stop-bit
 
 }
 
 void setup3(){
-	ADCSRA |= (1 << ADPS2);
-	ADCSRA |= (1 << ADPS1);
-	ADCSRA |= (1 << ADPS0);
 
-	ADMUX  |= (1 << REFS0);
-	ADCSRA |= (1 << ADEN);
+	ADMUX  |= (1 << REFS0); // Definindo tensão de referencia do conversor
+	ADCSRA |= (1 << ADEN);	// Habilitando conversor
 
 }
 
 float read_adc(uint8_t channel){
-	ADMUX &= 0xE0;           //Clear bits MUX0-4
-	ADMUX |= channel&0x07;   //Defines the new ADC channel to be read by setting bits MUX0-2
-	ADCSRB = channel&(1<<3); //Set MUX5
-	ADCSRA |= (1<<ADSC);      //Starts a new conversion
-	while(ADCSRA & (1<<ADSC));  //Wait until the conversion is done
-	return ADCW;
+	ADMUX |= channel&0x07;   	// Definindo pino de entrada (A5 NO ARDUINO)
+	ADCSRB = channel&(1<<3); 	// Definindo bit MUX5 como 0
+	ADCSRA |= (1<<ADSC);      	// Inicia nova conversão
+	while(ADCSRA & (1<<ADSC));  // Aguarda a conversão finalizar e retorna
+	return ADCW;				// Dois bytes com a conversão
 
-}         //Returns the ADC value of the chosen channel
+}
 
 
 int RMS (int repeat){
@@ -73,123 +69,77 @@ int RMS (int repeat){
 	for (int i = 0; i < repeat; i++){
 		digital_value = read_adc(channel);
 		accumulated = accumulated + (digital_value*digital_value);
-
-
 	}
 
 	average = accumulated/repeat;
-
 	return sqrt(average);
-
-
 }
 void exec3(){
 	setup3();
 	setup2();
+	while (true){
+		_delay_ms(2000.0);
+		float val = RMS(300);
+		float analog_val = (val*5)/1024;
+		printf("%.f", double(val));
+		char a[10] = "Digital: ";
+		char b[12] = "Analogico: ";
+		char digital[50];
+		char analog[50];
 
-	float val = RMS(30);
-	float analog_val = (val*5)/1024;
-	printf("%.f", double(val));
-	char a[10] = "Digital: ";
-	char b[12] = "Analogico: ";
-	char digital[50];
-	char analog[50];
-	for (int i = 0; i < 9; i ++){
+		for (int i = 0; i < 9; i ++){			// Loop para escrever na serial
+			while (!( UCSR0A & (1<<UDRE0)));	// A palavra "Digital: "
+			UDR0 = (uint8_t) a[i];
+		}
 
-		while ( !( UCSR0A & (1<<UDRE0)) )  ;
-		UDR0 = (uint8_t) a[i];
+		dtostrf(val,5,1, digital);
+		dtostrf(analog_val, 3,3,analog);
 
+		for (int i = 0; i < 5; i ++){			 // Loop para escrever os bytes
+			while (!( UCSR0A & (1<<UDRE0)));     // Da conversão em valores
+			UDR0 = (uint8_t) digital[i];		 // Digitais
+		}
 
-	}
+		for (int i = 0; i < 11; i ++){			 // Loop para escrever na serial
+			while (!( UCSR0A & (1<<UDRE0)));     // A palavra "Analogico: "
+			UDR0 = (uint8_t) b[i];
+		}
 
-	dtostrf(val,5,1, digital);
-	dtostrf(analog_val, 3,3,analog);
-
-	for (int i = 0; i < 5; i ++){
-
-				while ( !( UCSR0A & (1<<UDRE0)) )  ;
-				UDR0 = (uint8_t) digital[i];
-
-
-			}
-
-	for (int i = 0; i < 11; i ++){
-
-		while ( !( UCSR0A & (1<<UDRE0)) )  ;
-		UDR0 = (uint8_t) b[i];
-
-
-	}
-
-	for (int i = 0; i < 4; i ++){
-
-					while ( !( UCSR0A & (1<<UDRE0)) )  ;
-					UDR0 = (uint8_t) analog[i];
-
-
-
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
-void controleBotao5V(){
-	// O led conectado no bit 6 da PORTH irá acender
-	// Quando o quinto bit da PORT4 estiver em 1
-	if (PINE & (1<<5)){
-		PORTH |= (1 << 6);
-
-	}
-	else {
-		PORTH &= ~(1 << 6);
+		for (int i = 0; i < 4; i ++){			 // Loop para escrever os bytes
+			while (!( UCSR0A & (1<<UDRE0)));     // Da conversão em valores
+			UDR0 = (uint8_t) analog[i];			 // Analógicos
+		}
 	}
 
 }
 
-void controleBotao0V(){
-	// O led conectado no bit 5 da PORTH irá acender
-	// Quando o quarto bit da PORT4 estiver em 0
-	if (PINE & (1<<4)){
-		PORTH &= ~(1 << 5);
 
-	}
-	else {
-		PORTH |= (1 << 5);
-	}
-
-}
 void exec1(){
-
 	setup1();
 	while(true){
-		controleBotao5V();
-		controleBotao0V();
+		if ((PINE & (1<<PE5))){ 	// O led conectado no bit 6 da PORTH
+			PORTH |= (1 << PH6);    // Irá acender quando o quinto bit
+		} else {					// Da PORT4 estiver em 1
+			PORTH &= ~(1 << PH6);
+		}
+
+		if (PINE & (1<<PE4)){		// O led conectado no bit 5 da PORTH irá
+			PORTH &= ~(1 << PH5);   // Acender quando o quarto bit da PORT4
+		} else {					// Estiver em 0
+			PORTH |= (1 << PH5);
+		}
 	}
 }
 
 void exec2(){
 	setup2();
 	while (true){
-
 		unsigned char byte;
-		if ( (UCSR0A & (1<<RXC0)) ){
-			byte= UDR0;
-			byte = byte + 1;
-
-			if ( ( UCSR0A & (1<<UDRE0)) )   {
-				UDR0 = byte;
+		if ((UCSR0A & (1<<RXC0)) ){   			// Se receber algo pela serial
+			byte= UDR0;							// Incrementa o byte e envia
+			byte = byte + 1;					// O byte incrementado
+			if (( UCSR0A & (1<<UDRE0))){        // Só envia se não ter nada
+				UDR0 = byte;					// Na fila de envio
 			}
 		}
 	}
@@ -198,14 +148,15 @@ void exec2(){
 
 
 void setupINT(){
-	DDRE &= ~(1 << PE4); // PORT D2 AS INPUT
+	DDRE &= ~(1 << PE4);  // Pino D2 do arduino como entrada
 
-	DDRH |= (1 << PH6); // PORT D8 AS OUTPUT
-	UCSR0B = 0X00;
-	EICRB = (1 << ISC41);
-	EICRB = (1 << ISC40);
-	EIMSK = (1 << INT4);
-	sei();
+	DDRH |= (1 << PH6);   // Pino D9 do arduino como saida
+	UCSR0B = 0X00;		  // Desativando as portas seriais
+
+	EICRB = (1 << ISC41); // Definindo a interrupção externo INT4 para ser
+	EICRB = (1 << ISC40); // Acionanda na borda de subida do sinal
+	EIMSK = (1 << INT4);  // Habilitando a interrupção externa INT4
+	sei();				  // Habilitando o pino de interrupção global
 
 }
 void exec4(){
@@ -225,6 +176,7 @@ void handler_exec4(){
 }
 
 void handler_exec5(){
+
 	while(PINE & (1 << PE4)){
 		PORTH |= (1 << PH6);
 	}
@@ -241,9 +193,9 @@ bool debounce(){
 }
 ISR(INT4_vect){
 	if(debounce()){
-	//handler_exe4();
-	handler_exec5();     // trocar handlers de acordo com o exercicio
-}
+		//handler_exe4();
+		handler_exec5();     // trocar handlers de acordo com o exercicio
+	}
 }
 int main(){
 	//exec1();
@@ -252,8 +204,10 @@ int main(){
 
 	//exec3();
 	//exec4();
-	exec5();
+	//exec5();
 }
+
+
 
 
 
